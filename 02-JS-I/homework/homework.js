d070c5f8ca0e3d1e32a8f706a23405e304c13d7e
Alfred Manolo <alfredmanolovasquez@gmail.com>
2021-12-02 17:31:33 -0500

    homework corrected

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,11 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-      if ('str1.length' === 'str2.length') {
-        return true;
-      } else {
-        return false;
-      }
+      return ('str1.length' === 'str2.length') 
 }
 
 function menosQueNoventa(num) {
@@ -103,8 +99,8 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-      return x / y;
-}
+  return x % y;
+
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
@@ -161,8 +157,8 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-      var a = Math.random();
-      alert (a);
+      return Math.random();
+      
 }
 
 function esPositivo(numero) {
@@ -170,15 +166,18 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false 
-      if (numero === 0 ) {
-        return false;
-      } else if (numero  > 0 ) {
-        return "Es Positivo"; 
-      } else {
-        return "Es Negativo"
-      }
-
+  if(numero === 0) {
+    return false;
+  }
+  else if(numero > 0) {
+    return "Es positivo";
+  }
+  else {
+    return "Es negativo";
+  }
 }
+
+      
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
@@ -244,17 +243,15 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-            if(letra.length > 1){
-              return "Dato incorrecto"
-            }
-            if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-              return "Es vocal"
-            } else {
-                  return "Dato incorrecto"
-            }
+          
+    if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+      return "Es vocal"
+        } else {
+          return "Dato incorrecto"
+        }
             
 
-}
+
 
 
 
